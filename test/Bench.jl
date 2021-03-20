98e57825c075d7fc5009a337940d825f13ce0402
Alexander Voigt <ahgvoigt@gmail.com>
2021-03-20 13:05:59 +0100

    reduce benchmark output uncertainty

--- test/Bench.jl
+++ test/Bench.jl
@@ -1,8 +1,13 @@
 include("../src/Li2.jl")
 
-n = 1000_000
+n = 10_000_000
 z_min = -5
 z_max = 5
 data = (z_max - z_min)*rand(ComplexF64, n) + z_min*(1.0 + 1.0im)*ones(n)
 
+println("Benchmarking li2::ComplexF64")
+
+li2(1.0 + 1.0im) # trigger compilation
+@time map(li2, data)
+@time map(li2, data)
 @time map(li2, data)
