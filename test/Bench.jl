8d0cb8962f74d0680906c7209698570397c99145
Alexander Voigt <ahgvoigt@gmail.com>
2021-03-20 13:08:51 +0100

    benchmark real li2

--- test/Bench.jl
+++ test/Bench.jl
@@ -3,11 +3,19 @@ include("../src/Li2.jl")
 n = 10_000_000
 z_min = -5
 z_max = 5
-data = (z_max - z_min)*rand(ComplexF64, n) + z_min*(1.0 + 1.0im)*ones(n)
+cmpl_data = (z_max - z_min)*rand(ComplexF64, n) + z_min*(1.0 + 1.0im)*ones(n)
+real_data = map(real, cmpl_data)
+
+println("Benchmarking li2::Float64")
+
+map(li2, real_data)       # trigger compilation
+@time map(li2, real_data)
+@time map(li2, real_data)
+@time map(li2, real_data)
 
 println("Benchmarking li2::ComplexF64")
 
-map(li2, data)       # trigger compilation
-@time map(li2, data)
-@time map(li2, data)
-@time map(li2, data)
+map(li2, cmpl_data)       # trigger compilation
+@time map(li2, cmpl_data)
+@time map(li2, cmpl_data)
+@time map(li2, cmpl_data)
