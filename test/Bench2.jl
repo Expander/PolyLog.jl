f0c0ee7f83b26e77d5ac854320a160dfeb027687
Alexander Voigt <ahgvoigt@gmail.com>
2021-04-09 10:02:52 +0200

    adding second benchmark

---
+++ test/Bench2.jl
@@ -0,0 +1,17 @@
+using BenchmarkTools
+using PolyLog
+
+function benchmark_and_print(f, z, str)
+    println("----------------------------")
+    println("Benchmarking ", str)
+    println("----------------------------")
+    b = @benchmark $f($z)
+    io = IOBuffer()
+    show(io, "text/plain", b)
+    println(String(take!(io)))
+end
+
+benchmark_and_print(PolyLog.li2, 1.1        , "li2::Float64")
+benchmark_and_print(PolyLog.li2, 1.1 + 0.5im, "li2::ComplexF64")
+benchmark_and_print(PolyLog.li3, 1.1 + 0.5im, "li3::ComplexF64")
+benchmark_and_print(PolyLog.li4, 1.1 + 0.5im, "li4::ComplexF64")
