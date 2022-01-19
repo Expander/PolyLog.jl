573e02305e37def77b1f5a5f416090173d96e9d2
Alexander Voigt <ahgvoigt@gmail.com>
2022-01-19 12:32:24 +0100

    avoid one allocation

--- test/Bench.jl
+++ test/Bench.jl
@@ -6,58 +6,64 @@ z_max = 5
 cmpl_data = (z_max - z_min)*rand(ComplexF64, n) + z_min*(1.0 + 1.0im)*ones(n)
 real_data = map(real, cmpl_data)
 
+time_li2(data) = @time map(PolyLog.li2, data)
+time_li3(data) = @time map(PolyLog.li3, data)
+time_li4(data) = @time map(PolyLog.li4, data)
+time_li5(data) = @time map(PolyLog.li5, data)
+time_li6(data) = @time map(PolyLog.li6, data)
+
 println("Benchmarking li2::Float64")
 
 map(PolyLog.li2, real_data)       # trigger compilation
-@time map(PolyLog.li2, real_data)
-@time map(PolyLog.li2, real_data)
-@time map(PolyLog.li2, real_data)
+time_li2(real_data)
+time_li2(real_data)
+time_li2(real_data)
 
 println("Benchmarking li2::ComplexF64")
 
 map(PolyLog.li2, cmpl_data)       # trigger compilation
-@time map(PolyLog.li2, cmpl_data)
-@time map(PolyLog.li2, cmpl_data)
-@time map(PolyLog.li2, cmpl_data)
+time_li2(cmpl_data)
+time_li2(cmpl_data)
+time_li2(cmpl_data)
 
 println("Benchmarking li3::Float64")
 
 map(PolyLog.li3, real_data)       # trigger compilation
-@time map(PolyLog.li3, real_data)
-@time map(PolyLog.li3, real_data)
-@time map(PolyLog.li3, real_data)
+time_li3(real_data)
+time_li3(real_data)
+time_li3(real_data)
 
 println("Benchmarking li3::ComplexF64")
 
 map(PolyLog.li3, cmpl_data)       # trigger compilation
-@time map(PolyLog.li3, cmpl_data)
-@time map(PolyLog.li3, cmpl_data)
-@time map(PolyLog.li3, cmpl_data)
+time_li3(cmpl_data)
+time_li3(cmpl_data)
+time_li3(cmpl_data)
 
 println("Benchmarking li4::Float64")
 
 map(PolyLog.li4, real_data)       # trigger compilation
-@time map(PolyLog.li4, real_data)
-@time map(PolyLog.li4, real_data)
-@time map(PolyLog.li4, real_data)
+time_li4(real_data)
+time_li4(real_data)
+time_li4(real_data)
 
 println("Benchmarking li4::ComplexF64")
 
 map(PolyLog.li4, cmpl_data)       # trigger compilation
-@time map(PolyLog.li4, cmpl_data)
-@time map(PolyLog.li4, cmpl_data)
-@time map(PolyLog.li4, cmpl_data)
+time_li4(cmpl_data)
+time_li4(cmpl_data)
+time_li4(cmpl_data)
 
 println("Benchmarking li5::ComplexF64")
 
 map(PolyLog.li5, cmpl_data)       # trigger compilation
-@time map(PolyLog.li5, cmpl_data)
-@time map(PolyLog.li5, cmpl_data)
-@time map(PolyLog.li5, cmpl_data)
+time_li5(cmpl_data)
+time_li5(cmpl_data)
+time_li5(cmpl_data)
 
 println("Benchmarking li6::ComplexF64")
 
 map(PolyLog.li6, cmpl_data)       # trigger compilation
-@time map(PolyLog.li6, cmpl_data)
-@time map(PolyLog.li6, cmpl_data)
-@time map(PolyLog.li6, cmpl_data)
+time_li6(cmpl_data)
+time_li6(cmpl_data)
+time_li6(cmpl_data)
