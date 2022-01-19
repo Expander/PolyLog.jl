df569b2f188bd8de699689e8b8f48bb79150e8f6
Alexander Voigt <ahgvoigt@gmail.com>
2022-01-19 18:24:49 +0100

    formulation of inversion

--- src/Li.jl
+++ src/Li.jl
@@ -1,6 +1,18 @@
+# calculates remainder from inversion formula
+function li_rest(n::Integer, x::Float64)::Float64
+    0.0
+end
+
 function li(n::Integer, x::Float64)::Float64
     if n < 1
         throw(DomainError(n, "li(n,x) undefined for n < 1"))
+    end
+
+    # transformation on [-1,1]
+    (y, rest, sgn) = if (abs(x) <= 1.0)
+        (x, 0.0, 1.0)
+    else # abs(x) > 1
+        (inv(x), li_rest(n, x), iseven(n - 1) ? 1.0 : -1.0)
     end
 
     0.0
