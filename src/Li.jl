f7e257e821a4f33dbf960dfcdcf9dbaeb3351a03
Alexander Voigt <ahgvoigt@gmail.com>
2022-01-19 19:00:25 +0100

    adding naive series for Li(n,x)

--- src/Li.jl
+++ src/Li.jl
@@ -3,6 +3,19 @@ function li_rest(n::Integer, x::Float64)::Float64
     0.0
 end
 
+# naive series expansion of Li(n,x) for |x| < 1
+function li_series(n::Integer, x::Float64)::Float64
+    sum = x
+
+    for k in 2:typemax(n)
+        old_sum = sum
+        sum += x^k/Float64(k)^n
+        sum == old_sum && break
+    end
+
+    sum
+end
+
 function li(n::Integer, x::Float64)::Float64
     if n < 1
         throw(DomainError(n, "li(n,x) undefined for n < 1"))
@@ -15,5 +28,5 @@ function li(n::Integer, x::Float64)::Float64
         (inv(x), li_rest(n, x), iseven(n - 1) ? 1.0 : -1.0)
     end
 
-    0.0
+    li_series(n, x)
 end
