388a916984deb71cb11146ee7a839196f5a46e1e
Alexander Voigt <ahgvoigt@gmail.com>
2022-01-21 12:34:53 +0100

    return special cases with n = 0 and n = 1

--- src/Li.jl
+++ src/Li.jl
@@ -75,10 +75,12 @@ function li_series(n::Integer, x::Float64)::Float64
 end
 
 function li(n::Integer, x::Float64)::Float64
-    if n < 1
-        throw(DomainError(n, "li(n,x) undefined for n < 1"))
+    if n < 0
+        throw(DomainError(n, "li(n,x) not implemented for n < 0"))
     end
 
+    n == 0 && return x/(1 - x)
+    n == 1 && return -log(1.0 - x)
     x == 1.0 && return zeta(n)
     x == -1.0 && return (2.0^(1 - n) - 1.0)*zeta(n)
 
