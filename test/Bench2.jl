eb20a446a9158f705750c04821eeee62f1859eb8
Alexander Voigt <ahgvoigt@gmail.com>
2022-01-24 16:32:32 +0100

    adding extended benchmark

--- test/Bench2.jl
+++ test/Bench2.jl
@@ -2,9 +2,9 @@ using BenchmarkTools
 import PolyLog
 
 function benchmark_and_print(f, z, str)
-    println("----------------------------")
+    println("------------------------------------------")
     println("Benchmarking ", str)
-    println("----------------------------")
+    println("------------------------------------------")
     b = @benchmark $f($z)
     io = IOBuffer()
     show(io, "text/plain", b)
@@ -19,3 +19,9 @@ benchmark_and_print(PolyLog.li4, 1.1        , "li4::Float64")
 benchmark_and_print(PolyLog.li4, 1.1 + 0.5im, "li4::ComplexF64")
 benchmark_and_print(PolyLog.li5, 1.1 + 0.5im, "li5::ComplexF64")
 benchmark_and_print(PolyLog.li6, 1.1 + 0.5im, "li6::ComplexF64")
+
+for n in vcat(collect(2:4), [100, 1000, 1000_000])
+    benchmark_and_print(x -> PolyLog.li(n, x), -1.5, "li($(n), -1.5)::Float64")
+    benchmark_and_print(x -> PolyLog.li(n, x),  0.4, "li($(n), 0.4)::Float64")
+    benchmark_and_print(x -> PolyLog.li(n, x),  1.5, "li($(n), 1.5)::Float64")
+end
