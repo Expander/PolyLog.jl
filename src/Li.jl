7796f8f7fe11ad1ea7a8aef11b8468136b56f189
Alexander Voigt <ahgvoigt@gmail.com>
2022-03-01 15:58:10 +0100

    reorder functions for readability

--- src/Li.jl
+++ src/Li.jl
@@ -1,5 +1,146 @@
 const ComplexOrReal{T} = Union{T,Complex{T}}
 
+"""
+    li(n::Integer, x::Float64)::Float64
+
+Returns the real n-th order polylogarithm
+``\\Re[\\operatorname{Li}_n(x)]`` of a real number ``x`` of type
+`Float64` for all integers ``n``.
+
+The implementation for ``n < 0`` is an adaption of
+[[arxiv:2010.09860](https://arxiv.org/abs/2010.09860)].
+
+Author: Alexander Voigt
+
+License: MIT
+
+# Example
+```jldoctest; setup = :(using PolyLog)
+julia> li(10, 1.0)
+1.0009945751278182
+```
+"""
+function li(n::Integer, x::Float64)::Float64
+    isnan(x) && return NaN
+    isinf(x) && return -Inf
+    x == 0.0 && return 0.0
+    x == 1.0 && return zeta(n)
+    x == -1.0 && return neg_eta(n)
+
+    if n < 0
+        # arXiv:2010.09860
+        l2 = ln_sqr(x)
+        if 4*pi^2*x*x < l2
+            li_series_naive(n, x)
+        elseif l2 < 0.512*0.512*4*pi^2
+            real(li_series_unity_neg(n, Complex(x)))
+        else
+            oddsgn(n)*li_series_naive(n, inv(x))
+        end
+    elseif n == 0
+        li0(x)
+    elseif n == 1
+        li1(x)
+    elseif n == 2
+        li2(x)
+    elseif n == 3
+        li3(x)
+    elseif n == 4
+        li4(x)
+    else # n > 4
+        # transform x to [-1,1]
+        (x, rest, sgn) = if x < -1.0
+            (inv(x), li_rem(n, x), oddsgn(n))
+        elseif x < 1.0
+            (x, 0.0, 1.0)
+        else # x > 1.0
+            (inv(x), real(li_rem(n, Complex(x))), oddsgn(n))
+        end
+
+        li = if n < 20 && x > 0.75
+            li_series_unity_pos(n, x)
+        else
+            li_series_naive(n, x)
+        end
+
+        rest + sgn*li
+    end
+end
+
+"""
+    li(n::Integer, x::ComplexF64)::ComplexF64
+
+Returns the complex n-th order polylogarithm
+``\\operatorname{Li}_n(x)`` of a complex number ``z`` of type
+`ComplexF64` for all integers ``n``.
+
+The implementation for ``n < 0`` is an adaption of
+[[arxiv:2010.09860](https://arxiv.org/abs/2010.09860)].
+
+Author: Alexander Voigt
+
+License: MIT
+
+# Example
+```jldoctest; setup = :(using PolyLog)
+julia> li(10, Complex(1.0, 1.0))
+1.0009945751278182
+```
+"""
+function li(n::Integer, z::ComplexF64)::ComplexF64
+    isnan(z) && return NaN
+    isinf(z) && return -Inf
+    z == 0.0 && return 0.0
+    z == 1.0 && return zeta(n)
+    z == -1.0 && return neg_eta(n)
+
+    if n < 0
+        l2 = abs2(log(z))
+        if 4*pi^2*abs2(z) < l2
+            li_series_naive(n, z)
+        elseif l2 < 0.512*0.512*4*pi^2
+            li_series_unity_neg(n, z)
+        else
+            sqrtz = sqrt(z)
+            2.0^(n - 1)*(li(n, sqrtz) + li(n, -sqrtz))
+        end
+    elseif n == 0
+        li0(z)
+    elseif n == 1
+        li1(z)
+    elseif n == 2
+        li2(z)
+    elseif n == 3
+        li3(z)
+    elseif n == 4
+        li4(z)
+    elseif n == 5
+        li5(z)
+    elseif n == 6
+        li6(z)
+    elseif abs2(z) <= 0.75*0.75
+        li_series_naive(n, z)
+    elseif abs2(z) >= 1.4*1.4
+        oddsgn(n)*li_series_naive(n, 1.0/z) + li_rem(n, z)
+    else
+        li_series_unity_pos(n, z)
+    end
+end
+
+# returns |ln(x)|^2 for all x
+function ln_sqr(x::Float64)::Float64
+    if x < 0.0
+        log(-x)^2 + pi^2
+    elseif x == 0.0
+        NaN
+    else
+        log(x)^2
+    end
+end
+
+# returns -(-1)^n
+oddsgn(n) = isodd(n) ? 1.0 : -1.0
+
 # returns r.h.s. of inversion formula for complex z:
 #
 # Li(n,-z) + (-1)^n Li(n,-1/z)
@@ -145,145 +286,4 @@ function li_series_naive(n::Integer, z::ComplexOrReal)
     end
 
     sum
-end
-
-# returns |ln(x)|^2 for all x
-function ln_sqr(x::Float64)::Float64
-    if x < 0.0
-        log(-x)^2 + pi^2
-    elseif x == 0.0
-        NaN
-    else
-        log(x)^2
-    end
-end
-
-# returns -(-1)^n
-oddsgn(n) = isodd(n) ? 1.0 : -1.0
-
-"""
-    li(n::Integer, x::Float64)::Float64
-
-Returns the real n-th order polylogarithm
-``\\Re[\\operatorname{Li}_n(x)]`` of a real number ``x`` of type
-`Float64` for all integers ``n``.
-
-The implementation for ``n < 0`` is an adaption of
-[[arxiv:2010.09860](https://arxiv.org/abs/2010.09860)].
-
-Author: Alexander Voigt
-
-License: MIT
-
-# Example
-```jldoctest; setup = :(using PolyLog)
-julia> li(10, 1.0)
-1.0009945751278182
-```
-"""
-function li(n::Integer, x::Float64)::Float64
-    isnan(x) && return NaN
-    isinf(x) && return -Inf
-    x == 0.0 && return 0.0
-    x == 1.0 && return zeta(n)
-    x == -1.0 && return neg_eta(n)
-
-    if n < 0
-        # arXiv:2010.09860
-        l2 = ln_sqr(x)
-        if 4*pi^2*x*x < l2
-            li_series_naive(n, x)
-        elseif l2 < 0.512*0.512*4*pi^2
-            real(li_series_unity_neg(n, Complex(x)))
-        else
-            oddsgn(n)*li_series_naive(n, inv(x))
-        end
-    elseif n == 0
-        li0(x)
-    elseif n == 1
-        li1(x)
-    elseif n == 2
-        li2(x)
-    elseif n == 3
-        li3(x)
-    elseif n == 4
-        li4(x)
-    else # n > 4
-        # transform x to [-1,1]
-        (x, rest, sgn) = if x < -1.0
-            (inv(x), li_rem(n, x), oddsgn(n))
-        elseif x < 1.0
-            (x, 0.0, 1.0)
-        else # x > 1.0
-            (inv(x), real(li_rem(n, Complex(x))), oddsgn(n))
-        end
-
-        li = if n < 20 && x > 0.75
-            li_series_unity_pos(n, x)
-        else
-            li_series_naive(n, x)
-        end
-
-        rest + sgn*li
-    end
-end
-
-"""
-    li(n::Integer, x::ComplexF64)::ComplexF64
-
-Returns the complex n-th order polylogarithm
-``\\operatorname{Li}_n(x)`` of a complex number ``z`` of type
-`ComplexF64` for all integers ``n``.
-
-The implementation for ``n < 0`` is an adaption of
-[[arxiv:2010.09860](https://arxiv.org/abs/2010.09860)].
-
-Author: Alexander Voigt
-
-License: MIT
-
-# Example
-```jldoctest; setup = :(using PolyLog)
-julia> li(10, Complex(1.0, 1.0))
-1.0009945751278182
-```
-"""
-function li(n::Integer, z::ComplexF64)::ComplexF64
-    isnan(z) && return NaN
-    isinf(z) && return -Inf
-    z == 0.0 && return 0.0
-    z == 1.0 && return zeta(n)
-    z == -1.0 && return neg_eta(n)
-
-    if n < 0
-        l2 = abs2(log(z))
-        if 4*pi^2*abs2(z) < l2
-            li_series_naive(n, z)
-        elseif l2 < 0.512*0.512*4*pi^2
-            li_series_unity_neg(n, z)
-        else
-            sqrtz = sqrt(z)
-            2.0^(n - 1)*(li(n, sqrtz) + li(n, -sqrtz))
-        end
-    elseif n == 0
-        li0(z)
-    elseif n == 1
-        li1(z)
-    elseif n == 2
-        li2(z)
-    elseif n == 3
-        li3(z)
-    elseif n == 4
-        li4(z)
-    elseif n == 5
-        li5(z)
-    elseif n == 6
-        li6(z)
-    elseif abs2(z) <= 0.75*0.75
-        li_series_naive(n, z)
-    elseif abs2(z) >= 1.4*1.4
-        oddsgn(n)*li_series_naive(n, 1.0/z) + li_rem(n, z)
-    else
-        li_series_unity_pos(n, z)
-    end
 end
