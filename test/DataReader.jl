db7080fea267dbedc79ee25b0cab5686a3ac8f3c
Alexander Voigt <alexander.voigt@hs-flensburg.com>
2022-04-20 11:01:13 +0200

    rename local variables

--- test/DataReader.jl
+++ test/DataReader.jl
@@ -1,8 +1,9 @@
 import DelimitedFiles
 
+# read file row-wise into pairs of complex numbers
 function read_from(filename)
     data = DelimitedFiles.readdlm(filename)
-    input  = map(z -> Complex(z[1], z[2]), zip(data[:,1], data[:,2]))
-    output = map(z -> Complex(z[1], z[2]), zip(data[:,3], data[:,4]))
-    hcat(input, output)
+    z1 = map(z -> Complex(z[1], z[2]), zip(data[:,1], data[:,2]))
+    z2 = map(z -> Complex(z[1], z[2]), zip(data[:,3], data[:,4]))
+    hcat(z1, z2)
 end
