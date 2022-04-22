fd6ad47cc14b7680ddf21394ae63d73337b5b75a
Alexander Voigt <ahgvoigt@gmail.com>
2022-04-23 00:06:13 +0200

    allow calculation of li0 with reduced precision

--- src/Li0.jl
+++ src/Li0.jl
@@ -1,9 +1,9 @@
 """
-    li0(x::Float64)::Float64
+    li0(x::AbstractFloat)::AbstractFloat
 
 Returns the real 0th order polylogarithm
 ``\\Re[\\operatorname{Li}_0(x)]`` of a real number ``x`` of type
-`Float64`.
+`AbstractFloat`.
 
 Author: Alexander Voigt
 
@@ -15,16 +15,18 @@ julia> li0(0.5)
 1.0
 ```
 """
+li0(x::AbstractFloat) = convert(typeof(x), li0(Float64(x)))
+
 function li0(x::Float64)::Float64
     x/(1 - x)
 end
 
 """
-    li0(x::ComplexF64)::ComplexF64
+    li0(x::Complex)::Complex
 
 Returns the complex 0th order polylogarithm
 ``\\operatorname{Li}_0(z)`` of a complex number ``z`` of type
-`ComplexF64`.
+`Complex`.
 
 Author: Alexander Voigt
 
@@ -36,6 +38,8 @@ julia> li0(1.0 + 1.0im)
 -1.0 + 1.0im
 ```
 """
+li0(x::Complex) = convert(typeof(x), li0(ComplexF64(x)))
+
 function li0(z::ComplexF64)::ComplexF64
     z/(1 - z)
 end
