ff3d8e7a3be65fba841f82696a095bf30b38c067
Alexander Voigt <ahgvoigt@gmail.com>
2022-04-23 12:42:30 +0200

    adding tests for further numeric data types

--- test/Li0.jl
+++ test/Li0.jl
@@ -11,6 +11,10 @@
     test_function_on_data(z -> PolyLog.li0(ComplexF16(z)), cmpl_data, 1e-2, 1e-2)
     test_function_on_data(z -> PolyLog.li0(Float16(z)   ), real_data, 1e-2, 1e-2)
 
+    @test PolyLog.li0(2.0f0) ≈ -2.0f0
+    @test PolyLog.li0(2//1) == -2//1
+    @test PolyLog.li0(2) ≈ -2.0
+
     @test PolyLog.li0(1.0) == Inf
     @test isnan(PolyLog.li0(1.0 + 0.0im))
 end
