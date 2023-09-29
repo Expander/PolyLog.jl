cdc816d65f214e73dc535b2cb86c4c21897a7951
Alexander Voigt <ahgvoigt@gmail.com>
2023-09-29 05:55:02 +0200

    pass return type as parameter

--- src/Harmonic.jl
+++ src/Harmonic.jl
@@ -1,9 +1,9 @@
 # returns n-th harmonic number, n > 0
-function harmonic(n::Integer)::Float64
+function harmonic(n::Integer, T=Float64)
     if n <= 0
         throw(DomainError(n, "harmonic not implemented for n <= 0"))
     elseif n < 20
-        sum = one(Float64)
+        sum = one(T)
         for k in 2:n
             sum += inv(k)
         end
