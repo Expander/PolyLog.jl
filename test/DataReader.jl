9fa56c07683def7d0c28b255e12f0eb7352ea700
Alexander Voigt <alexander.voigt@hs-flensburg.com>
2023-10-04 10:35:39 +0200

    avoid a default parameter

--- test/DataReader.jl
+++ test/DataReader.jl
@@ -1,7 +1,7 @@
 import DelimitedFiles
 
-# read file row-wise into pairs of complex numbers
-function read_from(filename, T=Float64)
+# read file row-wise into pairs of complex numbers into type T
+function read_from(filename, T)
     data = DelimitedFiles.readdlm(filename, T)
     z1 = map(z -> Complex(z[1], z[2]), zip(data[:,1], data[:,2]))
     z2 = map(z -> Complex(z[1], z[2]), zip(data[:,3], data[:,4]))
