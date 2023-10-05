e3a88c1e160061425a91dc5e71d7791cf220191c
Alexander Voigt <ahgvoigt@gmail.com>
2023-10-05 11:50:11 +0200

    remove commented function

--- src/Li2.jl
+++ src/Li2.jl
@@ -62,29 +62,6 @@ function reli2_approx(x::BigFloat)::BigFloat
 end
 
 
-# approximation of Li2(z) for 0 < Re(z) < 1/2 and |z| < 1
-# todo(alex): benchmark this routine
-# function li2_approx(x::BigFloat)::BigFloat
-#     u = -log1p(-x)
-#     u2 = u*u
-
-#     c0 = inv(4*big(pi)^2)
-
-#     p = u2*c0^2
-#     sum = one(x)/72
-
-#     for n in 2:typemax(Int64)
-#         old_sum = sum
-#         sgn = iseven(n) ? -1 : 1
-#         sum += sgn*p/(2*n + 1)*zeta(2*n, typeof(x))
-#         sum == old_sum && break
-#         p *= u2*c0
-#     end
-
-#     u + u2*(-one(x)/4 + 2*u*sum)
-# end
-
-
 # series expansion of Li2(z) for |z| <= 1 and Re(z) <= 0.5
 # in terms of u = -log(1-z)
 function li2_approx(u::ComplexF32)::ComplexF32
