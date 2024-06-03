adefae572eab4bf3cb3aafacb64b0cc5a67e3a38
longemen3000 <longemen3000@gmail.com>
2024-06-03 01:15:08 -0400

    add ifelse in CRC too

--- ext/PolyLogChainRulesExt.jl
+++ ext/PolyLogChainRulesExt.jl
@@ -2,10 +2,10 @@ module PolyLogChainRulesExt
 
 using PolyLog
 using ChainRulesCore
-@scalar_rule PolyLog.reli4(x) PolyLog.reli3(x)/x
-@scalar_rule PolyLog.reli3(x) PolyLog.reli2(x)/x
-@scalar_rule PolyLog.reli2(x) PolyLog.reli1(x)/x
+@scalar_rule PolyLog.reli4(x) ifelse(iszero(x), one(x), PolyLog.reli3(x)/x)
+@scalar_rule PolyLog.reli3(x) ifelse(iszero(x), one(x), PolyLog.reli2(x)/x)
+@scalar_rule PolyLog.reli2(x) ifelse(iszero(x), one(x), PolyLog.reli1(x)/x)
 @scalar_rule PolyLog.reli1(x) one(x)/(one(x)-x)
-@scalar_rule PolyLog.reli(n,x) PolyLog.reli(n-1,x)/x
+@scalar_rule PolyLog.reli(n,x) ifelse(iszero(x), one(x), PolyLog.reli(n-1,x)/x)
 end #module
 
