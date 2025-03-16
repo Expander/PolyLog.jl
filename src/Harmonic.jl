f9155895b8dd7a601d8f78f216b908c3bbe6c3f8
Alexander Voigt <alexander.voigt@physik.rwth-aachen.de>
2025-03-16 20:12:28 +0100

    accept all types T that support inversion and summation

--- src/Harmonic.jl
+++ src/Harmonic.jl
@@ -1,30 +1,12 @@
 # returns n-th harmonic number, n > 0
 function harmonic(n::Integer, ::Type{T})::T where T
-    if issimplefloat(T)
-        convert(T, harmonic_f64(n))
-    else
-        harmonic_big(n)
-    end
-end
-
-function harmonic_f64(n::Integer)::Float64
     if n <= 0
         throw(DomainError(n, "harmonic not implemented for n <= 0"))
     elseif n == 1
-        one(Float64)
-    elseif n < 20
-        one(Float64) + sum(inv, 2:n)
+        one(T)
+    elseif issimplefloat(T) && n >= 20
+        convert(T, EULERGAMMA_F64 + digamma(n + 1))
     else
-        EULERGAMMA_F64 + digamma(n + 1)
-    end
-end
-
-function harmonic_big(n::Integer)::BigFloat
-    if n <= 0
-        throw(DomainError(n, "harmonic not implemented for n <= 0"))
-    elseif n == 1
-        one(BigFloat)
-    else
-        one(BigFloat) + sum(x -> inv(big(x)), 2:n)
+        one(T) + sum(x -> inv(T(x)), 2:n)
     end
 end
