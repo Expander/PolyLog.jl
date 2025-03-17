85ec03d844a1e004b391f3b7e793e87b69d575b2
Alexander Voigt <Expander@users.noreply.github.com>
2025-03-17 06:05:13 +0100

    Refactor hamonic function (#33)
    to accept all types T that support (multiplicative) inversion and summation. This leads to a performance improvement of `harmonic(19, Float64)` by a factor ~10.

--- src/Harmonic.jl
+++ src/Harmonic.jl
@@ -1,34 +1,12 @@
 # returns n-th harmonic number, n > 0
 function harmonic(n::Integer, ::Type{T})::T where T
-    if issimplefloat(T)
-        convert(T, harmonic_f64(n))
-    else
-        harmonic_big(n)
-    end
-end
-
-function harmonic_f64(n::Integer)::Float64
     if n <= 0
         throw(DomainError(n, "harmonic not implemented for n <= 0"))
-    elseif n < 20
-        sum = one(Float64)
-        for k in 2:n
-            sum += inv(k)
-        end
-        sum
+    elseif n == 1
+        one(T)
+    elseif issimplefloat(T) && n >= 20
+        convert(T, EULERGAMMA_F64 + digamma(n + 1))
     else
-        EULERGAMMA_F64 + digamma(n + 1)
-    end
-end
-
-function harmonic_big(n::Integer)::BigFloat
-    if n <= 0
-        throw(DomainError(n, "harmonic not implemented for n <= 0"))
-    else
-        sum = one(BigFloat)
-        for k in 2:n
-            sum += inv(big(k))
-        end
-        sum
+        one(T) + sum(x -> inv(T(x)), 2:n)
     end
 end
