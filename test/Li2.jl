6c833654e326ee007285d95cb0f2ab3d0a68981b
Alexander Voigt <alexander.voigt@physik.rwth-aachen.de>
2025-03-21 07:44:17 +0100

    adding alternative implementation of series expansion of li2
    from [Ginsberg, Zaborowski, "The Dilogarithm Function of a Real
    Argument"] for testing purposes.
    
    This alternative routine appears to be less performant than the taylor
    series for arguments of type BigFloat due to more costly BigFloat
    operations necessary in the loop.

--- test/Li2.jl
+++ test/Li2.jl
@@ -1,12 +1,45 @@
+# Accelerated series expansion of Li2(x) for x with |x| < 1
+# from [Ginsberg, Zaborowski, "The Dilogarithm Function of a Real Argument"]
+function li2_accel(x::T)::T where T
+    x2 = x*x
+    fx = 4*x
+    sum = fx*x2/36
+    xn = 4*x2*x2
+
+    for k in 2:typemax(Int64)
+        tk = oftype(x, k)
+        term = xn/(tk*(tk + one(x))*(tk + oftype(x, 2)))^2
+        !isfinite(term) && break
+        old_sum = sum
+        sum += term
+        sum == old_sum && break
+        xn *= x
+    end
+
+    (sum + fx + oftype(x, 23//4)*x2 + 3*(1 - x2)*log1p(-x))/(1 + fx + x2)
+end
+
+
+function filter_small(data, limit)
+    z1 = [data[i,1] for i in 1:size(data,1) if abs2(data[i,1]) < limit]
+    z2 = [data[i,2] for i in 1:size(data,1) if abs2(data[i,1]) < limit]
+    hcat(z1, z2)
+end
+
+
 @testset "li2" begin
     cmpl_data = read_from(joinpath(@__DIR__, "data", "Li2.txt"), BigFloat)
     real_data = filter_real(cmpl_data)
+    cmpl_data_less_1 = filter_small(cmpl_data, 0.99)
+    real_data_less_1 = filter_small(real_data, 0.99)
 
     setprecision(BigFloat, MAX_BINARY_DIGITS) do
         ep = 30*eps(BigFloat)
         test_function_on_data(PolyLog.li2  , cmpl_data, ep, ep)
+        test_function_on_data(li2_accel    , cmpl_data_less_1, ep, ep)
         ep = 10*eps(BigFloat)
         test_function_on_data(PolyLog.reli2, real_data, ep, ep)
+        test_function_on_data(li2_accel    , real_data_less_1, ep, ep)
     end
 
     test_function_on_data(PolyLog.li2  , map(ComplexF64, cmpl_data), 1e-14, 1e-14)
@@ -17,6 +50,15 @@
 
     test_function_on_data(PolyLog.li2  , filter_ComplexF16(map(ComplexF16, cmpl_data)), 1e-2, 1e-2)
     test_function_on_data(PolyLog.reli2, map(Float16, real_data), 1e-2, 1e-2)
+
+    test_function_on_data(li2_accel, map(ComplexF64, cmpl_data_less_1), 1e-14, 1e-14)
+    test_function_on_data(li2_accel, map(Float64   , real_data_less_1), 1e-14, 1e-14)
+
+    test_function_on_data(li2_accel, map(ComplexF32, cmpl_data_less_1), 1e-6, 1e-6)
+    test_function_on_data(li2_accel, map(Float32   , real_data_less_1), 1e-6, 1e-6)
+
+    test_function_on_data(li2_accel, filter_ComplexF16(map(ComplexF16, cmpl_data_less_1)), 1e-2, 1e-2)
+    test_function_on_data(li2_accel, map(Float16, real_data_less_1), 1e-2, 1e-2)
 
     # test signbit for 0.0 and -0.0 arguments
     for T in (Float16, Float32, Float64, BigFloat)
