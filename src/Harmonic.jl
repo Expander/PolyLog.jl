81c79874040e6610b745e677dc2e5f60ca0d02a8
Alexander Voigt <Expander@users.noreply.github.com>
2025-11-14 11:38:49 +0100

    Use builtin eulergamma constant (#36)

--- src/Harmonic.jl
+++ src/Harmonic.jl
@@ -5,7 +5,7 @@ function harmonic(n::Integer, ::Type{T})::T where T
     elseif n == 1
         one(T)
     elseif issimplefloat(T) && n >= 20
-        convert(T, EULERGAMMA_F64 + digamma(n + 1))
+        convert(T, Base.MathConstants.eulergamma + digamma(n + 1))
     else
         one(T) + sum(x -> inv(T(x)), 2:n)
     end
