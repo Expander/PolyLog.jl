a12822f0e5300c35f4f951d91e77e905a5add2f1
Alexander Voigt <ahgvoigt@gmail.com>
2026-03-23 07:26:28 +0100

    extend test to more orders

--- test/Dual.jl
+++ test/Dual.jl
@@ -1,14 +1,13 @@
 if isdefined(Base, :get_extension)
     import ForwardDiff
 
-    function li_dual(n::Integer, z::Complex)
-        z_dual = complex(ForwardDiff.Dual(real(z)), ForwardDiff.Dual(imag(z)))
-        PolyLog.li(n, z_dual)
-    end
-
+    to_dual(z::Complex) = Complex(ForwardDiff.Dual(real(z)), ForwardDiff.Dual(imag(z)))
 
     @testset "Dual" begin
         z = 0.5 + 0.8im
-        @test li_dual(2, z) ≈ PolyLog.li(2, z) rtol=1e-15
+
+        for n in -10:10
+            @test PolyLog.li(n, to_dual(z)) ≈ PolyLog.li(n, z) rtol=1e-14
+        end
     end
 end
